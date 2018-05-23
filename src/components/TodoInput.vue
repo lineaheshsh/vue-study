13b164a116b62b9d5487fb53008acfc09bd075ff
ZZangHo <lineaheshsh@naver.com>
2018-05-23 13:37:27 +0900

    Vue.js animation function add

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,29 @@
         <span class="addContainer" v-on:click="addTodo">
             <i class="addBtn fa fa-plus" aria-hidden="true"></i>
         </span>
+
+        <modal v-if="showModal" @close="showModal = false">
+            <h3 slot="header">
+                경고
+            </h3>
+            <span slot="body">
+                할 일을 입력하세요.
+            </span>
+            <span slot="footer" @click="showModal = false">
+                <button>Ok</button>
+            </span>
+        </modal>
     </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
   data() {
       return {
-          newTodoItem: ''
+          newTodoItem: '',
+          showModal: false
       }
   },
   methods: {
@@ -20,11 +35,16 @@ export default {
               var value = this.newTodoItem && this.newTodoItem.trim();
               this.$emit('add-todo', value);
               this.clearInput();
+          } else {
+              this.showModal = !this.showModal;
           }
       },
       clearInput() {
           this.newTodoItem = '';
       }
+  },
+  components: {
+      Modal: Modal
   }
 }
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-      <ul>
+      <transition-group name="list" tag="ul">
         <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
             <i class="checkBtn fa fa-check" aria-hidden="true"></i>
             {{ todoItem }}
@@ -8,7 +8,7 @@
                 <i class="fa fa-trash-o" aria-hidden="true"></i>
             </span>
         </li>
-      </ul>
+      </transition-group>
   </section>
 </template>
 
@@ -29,6 +29,24 @@ export default {
         padding-left: 0px;
         margin-top: 0;
         text-align: left;
+    }
+
+    .list-item {
+        display: inline-block;
+        margin-right: 10px;
+    }
+
+    .list-move {
+        transition: transform 1s;
+    }
+
+    .list-enter-active, .list-leave-active {
+        transition: all 1s;
+    }
+
+    .list-enter, .list-leave-to {
+        opacity: 0;
+        transform: translateY(30px);
     }
 
     li {
